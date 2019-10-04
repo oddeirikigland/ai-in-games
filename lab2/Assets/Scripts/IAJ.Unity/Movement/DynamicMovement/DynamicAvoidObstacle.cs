6b21deba2637b88923edccb9a758d3ddc46b9b12
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-10-04 10:24:49 +0100

    central ray with short whiskers

--- lab2/Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicAvoidObstacle.cs
+++ lab2/Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicAvoidObstacle.cs
@@ -29,9 +29,22 @@ namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
             normedVelocity.Normalize();
             if (normedVelocity.sqrMagnitude > 0)
             {
+                int vectorLenght = 6;
                 var auxPos = this.Character.Position + normedVelocity * 2.0f;
-                Ray RayVector = new Ray(auxPos, normedVelocity);
+                var leftWhisker = normedVelocity - new Vector3(0.5f,0,0);
+                var rightWhisker = normedVelocity - new Vector3(-0.5f,0,0);
+
+                Ray RayVector = new Ray(auxPos, normedVelocity * vectorLenght);
+                Ray LeftRayVector = new Ray(auxPos, leftWhisker * vectorLenght / 2);
+                Ray RightRayVector = new Ray(auxPos, rightWhisker * vectorLenght / 2);
+
+                Debug.DrawRay(auxPos, normedVelocity * vectorLenght, Color.black);
+                Debug.DrawRay(auxPos, leftWhisker * vectorLenght / 2, Color.black);
+                Debug.DrawRay(auxPos, rightWhisker * vectorLenght / 2, Color.black);
+                
                 collision = this.Obstacle.Raycast(RayVector, out hit, this.MaxLookAhead);
+                collision = this.Obstacle.Raycast(LeftRayVector, out hit, this.MaxLookAhead / 2);
+                collision = this.Obstacle.Raycast(RightRayVector, out hit, this.MaxLookAhead / 2);
                 if (!collision) return new MovementOutput();
                 base.Target.Position = hit.point + hit.normal * this.AvoidMargin;
                 return base.GetMovement();
