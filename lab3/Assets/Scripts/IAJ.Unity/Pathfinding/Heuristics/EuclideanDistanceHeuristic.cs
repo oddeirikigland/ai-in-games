e248be213906a31927ea27cc432dc67349129ee0
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-10-23 15:38:07 +0100

    adds gateway distance heuristic

--- lab3/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/EuclideanDistanceHeuristic.cs
+++ lab3/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/EuclideanDistanceHeuristic.cs
@@ -10,5 +10,11 @@ namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
             Vector3 delta = goalNode.Position - node.Position;
             return Mathf.Sqrt(delta.x * delta.x + delta.z * delta.z);
         }
+
+        public float H(NavigationGraphNode node, Vector3 gatewayPosition)
+        {
+            Vector3 delta = gatewayPosition - node.Position;
+            return Mathf.Sqrt(delta.x * delta.x + delta.z * delta.z);
+        }
     }
 }
---
+++ lab3/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/GatewayDistanceHeuristic.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures.HPStructures;
+using Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics;
+using RAIN.Navigation.Graph;
+using UnityEngine;
+
+public class GatewayDistanceHeuristic : IHeuristic
+{
+    public ClusterGraph ClusterGraph { get; set; }
+    public EuclideanDistanceHeuristic EuclideanDistanceHeuristic { get; set; }
+    public Cluster NodeCluster { get; set; }
+    public Cluster GoalNodeCluster { get; set; }
+    public float DistanceNodeToGoalNode { get; set; }
+
+    public GatewayDistanceHeuristic()
+    {
+        this.ClusterGraph = Resources.Load<ClusterGraph>("ClusterGraph");
+        this.EuclideanDistanceHeuristic = new EuclideanDistanceHeuristic(); 
+    }
+
+    public float H(NavigationGraphNode node, NavigationGraphNode goalNode)
+    {
+        this.NodeCluster = this.ClusterGraph.Quantize(node);
+        this.GoalNodeCluster = this.ClusterGraph.Quantize(goalNode);
+        this.DistanceNodeToGoalNode = float.PositiveInfinity;
+        float distance;
+
+        Vector3 nodeClusterPos = this.NodeCluster.Localize();
+        Vector3 goalNodeClusterPos = this.GoalNodeCluster.Localize();
+
+        if (nodeClusterPos != null && nodeClusterPos == goalNodeClusterPos)
+        {
+            return this.EuclideanDistanceHeuristic.H(node, goalNode);
+        }
+
+        foreach (Gateway nodeGateway in this.NodeCluster.gateways)
+        {
+            distance = this.EuclideanDistanceHeuristic.H(node, nodeGateway.Localize());
+            foreach (Gateway goalNodeGateway in this.GoalNodeCluster.gateways)
+            {
+                distance += this.ClusterGraph.gatewayDistanceTable[nodeGateway.id].entries[goalNodeGateway.id].shortestDistance;
+                distance += this.EuclideanDistanceHeuristic.H(goalNode, goalNodeGateway.Localize());
+                this.DistanceNodeToGoalNode = Mathf.Min(this.DistanceNodeToGoalNode, distance);
+            }
+        }        
+        return this.DistanceNodeToGoalNode;
+    }
+}
